c3a87eed1e4c067ec7e759f43f42e5564bdce723
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-04-08 22:02:30 +0900

    Added image and input support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from "./logo.svg";
-import "./App.css";
+import InstaPost from "./components/InstaPost";
+import { Container, Box, Input, Button, Center } from "@chakra-ui/react";
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [title, setTitle] = useState("Заголовок");
+  const ChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const [topic, setTopic] = useState("Тема");
+  const ChangeTopic = (e) => {
+    setTopic(e.target.value);
+  };
+  const [imgURL, setImgURL] = useState("");
+  const ChangeImage = (e) => {
+    setImgURL(e.target.value);
+  };
+
+  const widthRef = useRef(null);
+  const [squareWidth, setSquareWidth] = useState(0);
+  const [containerScale, setContainerScale] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSquareWidth(widthRef.current ? widthRef.current.offsetWidth : 0);
+      setContainerScale(squareWidth / 1920);
+    };
+
+    if (widthRef.current) {
+      setSquareWidth(widthRef.current ? widthRef.current.offsetWidth : 0);
+      setContainerScale(squareWidth / 1920);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [squareWidth, containerScale]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Haha Nice <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxW="4xl" py="6">
+      <Box boxShadow="2xl" rounded="xl" p="6" bg="white">
+        <Input placeholder="Ссылка на картинку" mb="3" onChange={ChangeImage} />
+        <Input placeholder="Заголовок" mb="3" onChange={ChangeTitle} />
+        <Input placeholder="Тема" onChange={ChangeTopic} />
+      </Box>
+      <Box boxShadow="2xl" rounded="xl" p="6" bg="white" mt="6">
+        <Box
+          w="100%"
+          h={squareWidth}
+          overflow="hidden"
+          boxShadow="outline"
+          ref={widthRef}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <InstaPost
+            title={title}
+            topic={topic}
+            image={imgURL}
+            scale={containerScale}
+          ></InstaPost>
+        </Box>
+      </Box>
+      <Center mt="6">
+        <Button colorScheme="red">Сохранить</Button>
+      </Center>
+    </Container>
   );
 }
 
---
+++ src/components/InstaPost/index.js
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "styled-components";
+
+const ImgContainer = styled.div`
+  left: 0;
+  width: 1920px;
+  height: 1920px;
+  position: relative;
+  font-family: "Open Sans", sans-serif;
+  transform-origin: 0 0;
+  transform: ${(props) => "scale(" + props.scale + ")"};
+  background: #e5e5e5;
+`;
+
+const MainImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 1;
+  display: ${(props) => (props.src ? "block" : "none")};
+`;
+
+const MainLogo = styled.img`
+  left: 1614px;
+  top: 70px;
+  position: absolute;
+  width: 241px;
+  height: 241px;
+  z-index: 3;
+  -webkit-filter: drop-shadow(0px 0px 40px rgba(0, 0, 0, 0.4));
+`;
+
+const TopicWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background: #079bd7;
+  text-align: left;
+  left: 0px;
+  top: 123px;
+  position: absolute;
+  width: auto;
+  height: 104px;
+  z-index: 4;
+  border-radius: 0 54px 54px 0;
+  border: 5px solid #ffffff;
+  border-left: none;
+  box-shadow: 0px 0px 40px rgba(0, 0, 0, 0.4);
+`;
+
+const TopicText = styled.h3`
+  font-weight: 700;
+  color: #ffffff;
+  font-size: 60px;
+  text-transform: uppercase;
+  line-height: 0.94;
+  padding-left: 50px;
+  padding-right: 50px;
+`;
+
+const TitleWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  z-index: 7;
+  height: auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 50px;
+  background: linear-gradient(
+    0deg,
+    rgba(7, 155, 215, 1) 25%,
+    rgba(255, 255, 255, 0) 100%
+  );
+`;
+
+const TitleText = styled.h1`
+  text-align: center;
+  font-weight: 700;
+  color: #ffffff;
+  font-size: 110px;
+  text-transform: uppercase;
+  line-height: 1.1;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-top: 150px;
+`;
+
+const InstaPost = ({ title, topic, image, scale }) => {
+  return (
+    <ImgContainer scale={scale}>
+      <MainImg src={image} alt="Укажите ссылку на картинку"></MainImg>
+      <MainLogo
+        src="https://i.postimg.cc/KvZ8g2pk/logo.png"
+        alt="Дентис"
+      ></MainLogo>
+      <TopicWrapper>
+        <TopicText>{topic}</TopicText>
+      </TopicWrapper>
+      <TitleWrapper>
+        <TitleText>{title}</TitleText>
+      </TitleWrapper>
+    </ImgContainer>
+  );
+};
+
+export default InstaPost;
