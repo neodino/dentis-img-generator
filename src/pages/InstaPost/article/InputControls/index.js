002cf3db0699c52e1eac9828c77c5e9c8647ea15
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-18 22:47:48 +0900

    Break down components

---
+++ src/pages/InstaPost/article/InputControls/index.js
@@ -0,0 +1,36 @@
+import { FormControl, FormLabel, IconButton, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import React from 'react';
+import { MdClose } from 'react-icons/md';
+
+const InputControls = ({ title, setTitle, topic, setTopic }) => {
+    return (
+        <>
+            <FormControl>
+                <FormLabel>Заголовок статьи</FormLabel>
+                <InputGroup>
+                    <Input variant='filled' value={title} placeholder='Введите текст...' onChange={e => setTitle(e.target.value)} />
+                    {title !== '' && <InputRightElement
+                        children={<IconButton
+                            variant='ghost'
+                            colorScheme='red'
+                            icon={<MdClose />}
+                            onClick={() => setTitle('')}></IconButton>} />}
+                </InputGroup>
+            </FormControl>
+            <FormControl>
+                <FormLabel>Тема статьи</FormLabel>
+                <InputGroup>
+                    <Input variant='filled' value={topic} placeholder='Введите текст...' onChange={e => setTopic(e.target.value)}></Input>
+                    {topic !== '' && <InputRightElement
+                        children={<IconButton
+                            variant='ghost'
+                            colorScheme='red'
+                            icon={<MdClose />}
+                            onClick={() => setTopic('')}></IconButton>} />}
+                </InputGroup>
+            </FormControl>
+        </>
+    );
+};
+
+export default InputControls;
--- src/pages/InstaPost/article/index.js
+++ src/pages/InstaPost/article/index.js
@@ -1,8 +1,8 @@
 import { Stack } from '@chakra-ui/layout';
-import { FormControl, FormLabel, IconButton, Input, InputGroup, InputRightElement, useColorModeValue } from '@chakra-ui/react';
+import { useColorModeValue } from '@chakra-ui/react';
 import React, { useState } from 'react';
-import { MdClose } from 'react-icons/md';
 import Composition from './Composition';
+import InputControls from './InputControls';
 
 const Article = () => {
 
@@ -16,31 +16,7 @@ const Article = () => {
             rounded='xl'
             bg={useColorModeValue('white', 'gray.700')}
         >
-            <FormControl>
-                <FormLabel>Заголовок статьи</FormLabel>
-                <InputGroup>
-                    <Input variant='filled' value={title} placeholder='Введите текст...' onChange={e => setTitle(e.target.value)} />
-                    {title !== '' && <InputRightElement
-                        children={<IconButton
-                            variant='ghost'
-                            colorScheme='red'
-                            icon={<MdClose />}
-                            onClick={() => setTitle('')}></IconButton>} />}
-                </InputGroup>
-            </FormControl>
-            <FormControl>
-                <FormLabel>Тема статьи</FormLabel>
-                <InputGroup>
-                    <Input variant='filled' value={topic} placeholder='Введите текст...' onChange={e => setTopic(e.target.value)}></Input>
-                    {topic !== '' && <InputRightElement
-                        children={<IconButton
-                            variant='ghost'
-                            colorScheme='red'
-                            icon={<MdClose />}
-                            onClick={() => setTopic('')}></IconButton>} />}
-                </InputGroup>
-            </FormControl>
-
+            <InputControls title={title} topic={topic} setTitle={setTitle} setTopic={setTopic} />
             <Composition title={title} topic={topic} />
         </Stack>
     );
